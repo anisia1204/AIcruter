c71219a84e255637704293af94d354b52aaca9d5
anisia1204 <anisia.rosu@student.upt.ro>
2025-04-02 22:52:32 +0300

    backend setup

---
+++ backend/aicruter/aicruter-web/src/test/java/com/testinprod/aicruter/AicruterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testinprod.aicruter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicruterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
