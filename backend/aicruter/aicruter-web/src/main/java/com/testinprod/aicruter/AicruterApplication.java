511747955e09fbd84818a95a080dcbf47630cd9b
anisia1204 <anisia.rosu@student.upt.ro>
2025-04-08 20:18:25 +0300

    fix

--- backend/aicruter/aicruter-web/src/main/java/com/testinprod/aicruter/AicruterApplication.java
+++ backend/aicruter/aicruter-web/src/main/java/com/testinprod/aicruter/AicruterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.testinprod.*")
-@ComponentScan(basePackages = "com.testinprod.*")
+@EntityScan(basePackages = "com.testinprod")
+@ComponentScan(basePackages = "com.testinprod")
 public class AicruterApplication {
 
 	public static void main(String[] args) {
