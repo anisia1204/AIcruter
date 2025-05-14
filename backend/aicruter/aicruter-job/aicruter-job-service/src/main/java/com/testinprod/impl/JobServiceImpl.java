6f6195905c37ac7cdc8afa875f16487465933a0c
anisia1204 <anisia.rosu@student.upt.ro>
2025-05-14 21:38:13 +0300

    [backend] save job

---
+++ backend/aicruter/aicruter-job/aicruter-job-service/src/main/java/com/testinprod/impl/JobServiceImpl.java
@@ -0,0 +1,43 @@
+package com.testinprod.impl;
+
+import com.testinprod.CompanyService;
+import com.testinprod.JobService;
+import com.testinprod.dto.JobDTO;
+import com.testinprod.dto.JobDTOMapper;
+import com.testinprod.entity.Job;
+import com.testinprod.entity.JobStatus;
+import com.testinprod.repository.JobJPARepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class JobServiceImpl implements JobService {
+    private final JobJPARepository jpaRepository;
+    private final JobDTOMapper jobDTOMapper;
+    private final CompanyService companyService;
+
+    public JobServiceImpl(JobJPARepository jpaRepository, JobDTOMapper jobDTOMapper, CompanyService companyService) {
+        this.jpaRepository = jpaRepository;
+        this.jobDTOMapper = jobDTOMapper;
+        this.companyService = companyService;
+    }
+
+    @Override
+    @Transactional
+    public JobDTO save(JobDTO jobDTO) {
+        Job job = jobDTOMapper.getEntityFromDTO(jobDTO);
+
+        job.setCreatedAt(LocalDateTime.now());
+        job.setStatus(JobStatus.OPEN);
+        job.setCompany(companyService.getById(jobDTO.getCompanyId()));
+
+        persist(job);
+        return jobDTOMapper.getDTOFromEntity(job);
+    }
+
+    private void persist(Job job) {
+        jpaRepository.save(job);
+    }
+}
