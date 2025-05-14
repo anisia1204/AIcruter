1230bc078965b98ccdaa8624f323c336bccb4dac
adrian.berejnec <adrianberejnec29@gmail.com>
2025-05-14 21:49:58 +0300

    Remove testing useless code

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,25 +1,10 @@
 "use client";
 
-import Link from "next/link";
-import { Briefcase, User } from "lucide-react";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { Briefcase, User } from "lucide-react";
+import Link from "next/link";
 
 export default function HomePage() {
-  useEffect(() => {
-    fetch("http://localhost:8080/api/applicant", {
-      headers: {
-        "ngrok-skip-browser-warning": "69420",
-      },
-    })
-      .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch");
-        return res.text();
-      })
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error fetching applicant:", err));
-  }, []);
-
   return (
     <div className="py-24">
       <div className="container mx-auto text-center px-4">
