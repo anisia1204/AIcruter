923a486370ed6290797ccf0aa365ecede0736749
anisia1204 <anisia.rosu@student.upt.ro>
2025-05-19 23:27:30 +0300

    [backend] get employee by user account id

--- backend/aicruter/aicruter-company/aicruter-company-domain/src/main/java/com/testinprod/repository/EmployerJPARepository.java
+++ backend/aicruter/aicruter-company/aicruter-company-domain/src/main/java/com/testinprod/repository/EmployerJPARepository.java
@@ -4,4 +4,5 @@ import com.testinprod.entity.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployerJPARepository extends JpaRepository<Employer, Long> {
+    Employer findByUserAccountId(Long id);
 }
